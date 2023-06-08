3ca0950f0cf7cb7abc2b4a6efc39d866b7d45983
Patricio Colosimo <colosimopatricio@gmail.com>
2023-06-08 19:57:23 -0300

    detalles finales

--- main.js
+++ main.js
@@ -22,40 +22,48 @@ form.addEventListener("submit", function (event) {
 
 const selectModelo = document.getElementById("modelo");
 
-// Datos del JSON
 const data = [
-  { "id": 1, "nombre": "Toyota" },
-  { "id": 2, "nombre": "Ford" },
-  { "id": 3, "nombre": "Chevrolet" },
-  { "id": 4, "nombre": "Honda" },
-  { "id": 5, "nombre": "Volkswagen" },
-  { "id": 6, "nombre": "BMW" },
-  { "id": 7, "nombre": "Mercedes-Benz" },
-  { "id": 8, "nombre": "Audi" },
-  { "id": 9, "nombre": "Nissan" },
-  { "id": 10, "nombre": "Hyundai" },
-  { "id": 11, "nombre": "Kia" },
-  { "id": 12, "nombre": "Mazda" },
-  { "id": 13, "nombre": "Subaru" },
-  { "id": 14, "nombre": "Lexus" },
-  { "id": 15, "nombre": "Volvo" },
-  { "id": 16, "nombre": "Jeep" },
-  { "id": 17, "nombre": "Mitsubishi" },
-  { "id": 18, "nombre": "Porsche" },
-  { "id": 19, "nombre": "Ferrari" },
-  { "id": 20, "nombre": "Lamborghini" }
+  { id: 1, nombre: "Toyota" },
+  { id: 2, nombre: "Ford" },
+  { id: 3, nombre: "Chevrolet" },
+  { id: 4, nombre: "Honda" },
+  { id: 5, nombre: "Volkswagen" },
+  { id: 6, nombre: "BMW" },
+  { id: 7, nombre: "Mercedes-Benz" },
+  { id: 8, nombre: "Audi" },
+  { id: 9, nombre: "Nissan" },
+  { id: 10, nombre: "Hyundai" },
+  { id: 11, nombre: "Kia" },
+  { id: 12, nombre: "Mazda" },
+  { id: 13, nombre: "Subaru" },
+  { id: 14, nombre: "Lexus" },
+  { id: 15, nombre: "Volvo" },
+  { id: 16, nombre: "Jeep" },
+  { id: 17, nombre: "Mitsubishi" },
+  { id: 18, nombre: "Porsche" },
+  { id: 19, nombre: "Ferrari" },
+  { id: 20, nombre: "Lamborghini" },
 ];
 
-// Generar las opciones del select
-data.forEach(item => {
+data.forEach((item) => {
   const option = document.createElement("option");
   option.value = item.id;
   option.textContent = item.nombre;
   selectModelo.appendChild(option);
 });
 
-
-
-
-
+fetch('data.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+  
+    var coberturaContainer = document.getElementById("cobertura-container");
+    coberturaContainer.innerHTML = ""; 
     
+    data.coberturas.forEach(function(cobertura) {
+      var coberturaElement = document.createElement("div");
+      coberturaElement.textContent = "Cobertura: " + cobertura.nombre + ", Precio: " + cobertura.precio;
+      coberturaContainer.appendChild(coberturaElement);
+    });
+  });
