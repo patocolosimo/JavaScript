068627c9c11e1d2dd9133b08a126ecd4a458d90b
Patricio Colosimo <colosimopatricio@gmail.com>
2023-06-07 21:38:51 -0300

    p.f.

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-
 const valorInput = document.getElementById("valor");
 
-  valorInput.addEventListener("input", calcularCotizacion);
+valorInput.addEventListener("input", calcularCotizacion);
+
+function calcularCotizacion() {
+  const valor = parseFloat(valorInput.value);
+
+  const cotizacion = valor * 0.2;
+
+  const precioCotizacionSpan = document.getElementById("precio-cotizacion");
+
+  precioCotizacionSpan.textContent = "$" + cotizacion;
+}
+
+const form = document.getElementById("cotizacionForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  calcularCotizacion();
+});
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
+
+  $(document).ready(function() {
+    $.getJSON('data.json', function(data) {
+      var dropdownOptions = $('#dropdownOptions');
   
-  function calcularCotizacion() {
-    const valor = parseFloat(valorInput.value);
+      data.forEach(function(opcion) {
+        var optionLink = $('<a>').addClass('dropdown-item').attr('href', '#').text(opcion.nombre);
+        dropdownOptions.append(optionLink);
+      });
+    });
+  });
+  
 
-    const cotizacion = valor * 0.2;
 
-    const precioCotizacionSpan = document.getElementById("precio-cotizacion");
-
-    precioCotizacionSpan.textContent = "$" + cotizacion;
-  }
-  const form = document.getElementById("cotizacionForm");
-
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    calcularCotizacion();});
+    
